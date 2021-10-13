84e47254bc9893de36b6f33408d83a49ad5bc676
anikdhar11 <aniksutradhar11@gmail.com>
2021-10-13 08:18:48 +0600

    intial commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyBjZwmsPjqPiUXUXITum2uQGw-PekYTG9Y",
+    authDomain: "ema-jon-shop-110bf.firebaseapp.com",
+    projectId: "ema-jon-shop-110bf",
+    storageBucket: "ema-jon-shop-110bf.appspot.com",
+    messagingSenderId: "815541326262",
+    appId: "1:815541326262:web:d41bdf48842b6a49f9f117"
 };
 
 export default firebaseConfig;
